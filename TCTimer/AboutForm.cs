5986b240120a0f94be20c7b1016f61eb11a1fbef
Fedox <fedox@fedox.pl>
2020-08-13 14:05:41 +0200

    About menu

---
+++ TCTimer/AboutForm.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace TCTimer
+{
+    public partial class AboutForm : Form
+    {
+        private int _numberOfClicks;
+
+        public AboutForm()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            _numberOfClicks++;
+            if (_numberOfClicks == 7)
+                MessageBox.Show(
+                    "Ale sobie kliknąłeś, no fajnie tak nie? Jakiś easter egg taki, co? Nie spodziwałeś się tego wcale",
+                    "Halo kartus", MessageBoxButtons.OK);
+        }
+    }
+}
--- TCTimer/TimerUI.cs
+++ TCTimer/TimerUI.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.Windows.Forms;
 
 namespace TCTimer
 {
@@ -7,6 +8,17 @@ namespace TCTimer
         public TimerForm()
         {
             InitializeComponent();
+        }
+
+        private void closeMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void aboutMenuItem_Click(object sender, EventArgs e)
+        {
+            var about = new AboutForm();
+            about.Show();
         }
     }
 }
